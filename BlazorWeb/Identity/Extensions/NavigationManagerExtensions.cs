5152423cc7b103cdd565d9ac6820cb651d86aa5f
Mackinnon Buck <mackinnon.buck@gmail.com>
2023-09-15 17:31:40 -0700

    External authentication pages

---
+++ BlazorWeb/Identity/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Microsoft.AspNetCore.Components;
+
+internal static class NavigationManagerExtensions
+{
+    public static void ReloadWithMessage(this NavigationManager navigationManager, string message)
+    {
+        var uri = GetUriWithMessageQueryParameter(navigationManager, newRelativeUri: null, message);
+        navigationManager.NavigateTo(uri, forceLoad: true);
+    }
+
+    public static void NavigateToWithMessage(this NavigationManager navigationManager, string? relativeUri, string? message)
+    {
+        var uri = GetUriWithMessageQueryParameter(navigationManager, relativeUri, message);
+        navigationManager.NavigateTo(uri);
+    }
+
+    private static string GetUriWithMessageQueryParameter(NavigationManager navigationManager, string? newRelativeUri, string? message)
+    {
+        var uriBuilder = new UriBuilder(navigationManager.Uri);
+
+        if (newRelativeUri is not null)
+        {
+            uriBuilder.Path = newRelativeUri;
+        }
+
+        if (message is not null)
+        {
+            uriBuilder.Query = $"Message={message}";
+        }
+
+        return uriBuilder.ToString();
+    }
+}
